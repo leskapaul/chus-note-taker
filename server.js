0c290d308a91596f722ca61ddfc91d6cd6f49dff
Paul Pollack <ppollack@yahoo-inc.com>
2020-05-31 21:31:28 -0400

    .

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const path = require ("path");
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, function(){
+  console.log("http://localhost:" + PORT);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Displays all characters
+//app.get("/api/characters", function(req, res) {
+ // return res.json(characters);
+//});
